a7fff459c47fef8e3a29a5b7c41d1638baa0af21
Dhananjay <Dhananjay@LAPTOP-JKO2NAK2>
2023-04-09 15:27:02 +0530

    Version 3 Update1

--- TestJava1/src/javaTest1/Test1.java
+++ TestJava1/src/javaTest1/Test1.java
@@ -3,5 +3,6 @@ package javaTest1;
 public class Test1 {
 public static void main(String[] args) {
 	System.out.println("Application Version 1");
+	System.out.println("Application Initialized");
 }
 }
